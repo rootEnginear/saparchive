9505bcf764d0da94ca18114ea86035d27abe3bbc
rootEnginear <rootenginear@users.noreply.github.com>
2025-06-21 21:27:05 +0700

    feat: update data

--- src/components/LandingSearch.tsx
+++ src/components/LandingSearch.tsx
@@ -35,7 +35,7 @@ export const LandingSearch = component$(() => {
             }}
           />
           <span class="label-text text-xs/1.5">
-            ชุดที่ 26 <span class="hidden 2xs:inline">(1.6Mb)</span>
+            ชุดที่ 26 <span class="hidden 2xs:inline">(1.8Mb)</span>
           </span>
         </label>
       </div>
